4db4a6673446178e35ed3eb72aaa673ca7f5f94a
smedberg <smedberg@gmail.com>
2021-12-06 14:58:55 -0800

    Adding a header and footer

--- src/App.js
+++ src/App.js
@@ -6,8 +6,14 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
+      <div>
+        This is the header
+      </div>
       <div id="surveyElement" />
       <div id="eligibilityResultsContainer" />
+      <div>
+        This is the footer
+      </div>
     </div>
   );
 }
