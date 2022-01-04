258f0875df241b7e989e6dd90fdf10e326e11b6c
smedberg <smedberg@gmail.com>
2022-01-04 14:07:22 -0800

    Moving state info up to App, etc.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
 import './App.css';
 import './survey.css';
 import './eligibility_results.css';
-import React from 'react'; 
+import React from 'react';
+import * as Survey from "survey-react";
+import {surveyJSON} from './survey_metadata.js';
+import {getEligibilityMatch} from './eligibility_checker.js'
+import {EligibilitiesDisplay} from './eligibilities_display.js'
+import {Controls} from './controls.js'
 
+const SHOW_SURVEY = 1;
+const SHOW_RESULTS = 2;
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-function App() {
-  return (
-    <div className="App">
-      <div>
-        This is the header
+    this.onSurveyComplete = this.onSurveyComplete.bind(this);
+    this.onShowSurvey = this.onShowSurvey.bind(this);
+
+    this.state = { displayState: SHOW_SURVEY, surveyResults: null};
+
+    this.survey = new Survey.Model(surveyJSON);
+    this.survey
+      .onComplete
+      .add(this.onSurveyComplete);
+
+  }
+
+  onSurveyComplete(results) {
+    this.setState({displayState: SHOW_RESULTS, surveyResults: results});
+  }
+
+  onShowSurvey() {
+    this.survey.clear();
+    this.survey.render();
+    this.setState({displayState: SHOW_SURVEY, surveyResults: null});
+  }
+
+  render() {
+    const hideSurvey = (this.state.displayState !== SHOW_SURVEY);
+    const hideResults = (this.state.displayState !== SHOW_RESULTS);
+
+    let eligibilities = null;
+    let values = null;
+    if (!hideResults) {
+      let eligibilityMatch = getEligibilityMatch(this.state.surveyResults.valuesHash);
+      if (eligibilityMatch !== null) {
+        eligibilities = eligibilityMatch.eligibilities;
+        values = this.state.surveyResults.valuesHash;
+      } else {
+        console.log('In App.render, did not find eligibility match ');
+        // No matches- display warning
+      }
+    }
+
+    return <div className="App">
+        <div>
+          This is the header
+        </div>
+        <div id="surveyElement">
+          <Survey.Survey model={this.survey} hidden={hideSurvey}/>
+        </div>
+        <div id="eligibilityResults">
+          <EligibilitiesDisplay hidden={hideResults} eligibilities={eligibilities} values={values}/>
+        </div>
+        <div id="controls">
+          <Controls onShowSurvey={this.onShowSurvey} onShowLanguage={this.onShowLanguage} hideRestart={hideResults}/>
+        </div>
+        <div>
+          This is the footer
+        </div>
       </div>
-      <div id="surveyElement" />
-      <div id="eligibilityResults" />
-      <div id="controls" />
-      <div>
-        This is the footer
-      </div>
-    </div>
-  );
+  };
 }
 
 export default App;
